800c7f7f3985faf8b3f28cb3d346596f2c26f4ba
phoet <phoetmail@googlemail.com>
2014-07-06 17:59:22 -0400

    fix the mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 describe UserMailer do
   describe "new_user" do
-    let(:mail) { UserMailer.new_user }
+    let(:user) { Fabricate.build(:user) }
+    let(:mail) { UserMailer.signup(user) }
 
     it "renders the headers" do
-      mail.subject.should eq("New user")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should_not be_nil
+      mail.to.should eq([user.email])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("validate")
     end
   end
 
