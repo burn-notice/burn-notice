96fdac411d51164be8ada4703ab936744f7a9677
phoet <phoetmail@googlemail.com>
2014-07-20 20:10:55 -0400

    add some nav to blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,22 @@ class ArticlesController < ApplicationController
   layout 'blog'
 
   def index
-    @articles = Article.active
+    @articles = search_scope
+    tags = Article.active.pluck(:tags)
+    @tags_count = tags.flatten.each_with_object(Hash.new(0)) { |tag, counts| counts[tag] += 1 }
+    ranges = {
+      'this year'   => 1.year.ago,
+      'this month'  => 1.month.ago,
+      'this week'   => 1.week.ago,
+    }
+    @dates_count = ranges.each_with_object(Hash.new(0)) { |(name, start), counts| counts[name] += Article.active.where('published_at < ?', start).count }
+  end
+
+  private
+
+  def search_scope
+    scope = Article.active.page(params[:page])
+    scope = scope.where('? = ANY (tags)', params[:tag]) if params[:tag]
+    scope
   end
 end
