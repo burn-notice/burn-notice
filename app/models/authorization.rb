bfea29e6c1cbc047c634448c95c2bf925ad608cf
phoet <phoetmail@googlemail.com>
2014-07-27 23:48:04 -0400

    remove some trash

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -2,5 +2,5 @@ class Authorization < ActiveRecord::Base
   belongs_to :user
 
   validates :uid, :provider, presence: true
-  validates :uid, uniqueness: { scope: :provider }
+  validates :uid, uniqueness: {scope: :provider}
 end
