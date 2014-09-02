097b2593316be0f09c03dea013e78e4748f82bcf
phoet <phoetmail@googlemail.com>
2014-09-02 20:59:59 +0200

    move stuff to sessions controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -35,34 +35,4 @@ class PublicController < ApplicationController
       end
     end
   end
-
-  def signup
-    @auth = session[:auth_data]
-    check_existing_user(@auth['info']['email'])
-    @user = User.new(nickname: @auth['info']['nickname'], email: @auth['info']['email'])
-    @user.authorizations.build provider: @auth['provider'], uid: @auth['uid']
-  end
-
-  def complete
-    @user = User.new(params.require(:user).permit!)
-    if @user.save
-      session.delete(:auth_data)
-      mail = UserMailer.signup(@user)
-      MailerJob.new.async.deliver(mail)
-      sign_in(@user)
-      redirect_to session.delete(:auth_path), notice: "Hi #{@user.nickname}, welcome to Burn-Notice!"
-    else
-      check_existing_user(params[:user][:email])
-      render :signup
-    end
-  end
-
-  private
-
-  def check_existing_user(email)
-    if existing_user = User.find_by_email(email)
-      providers = existing_user.authorizations.map(&:provider)
-      flash.now[:alert] = "An existing user was found for #{email}, please sign in through #{providers.to_sentence}!"
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,22 +27,21 @@ Rails.application.routes.draw do
   resource :sitemap, only: :show
 
   scope '/p' do
-    get  '/open/:token',  to: 'public#open',      as: :open
-    post '/read',         to: 'public#read',      as: :read
+    get  '/open/:token',  to: 'public#open',  as: :open
+    post '/read',         to: 'public#read',  as: :read
   end
 
   scope '/auth' do
-    get '/offline_login/:nickname',  to: 'sessions#offline_login' if Rails.env.development?
-    get '/:provider/callback',       to: 'sessions#create'
-    get '/failure',                  to: 'sessions#failure'
-    get '/destroy_session',          to: 'sessions#destroy',  as: :logout
-    get '/validation/:token',        to: 'sessions#validation', as: :validation
+    get  '/offline_login/:nickname', to: 'sessions#offline_login' if Rails.env.development?
+    get  '/:provider/callback',      to: 'sessions#create'
+    get  '/failure',                 to: 'sessions#failure'
+    get  '/destroy_session',         to: 'sessions#destroy',    as: :logout
+    get  '/validation/:token',       to: 'sessions#validation', as: :validation
+    get  '/signup',                  to: 'sessions#signup',     as: :signup
+    post '/complete',                to: 'sessions#complete',   as: :complete
   end
 
   root 'beta_users#index'
-
-  get  '/signup',       to: 'public#signup',    as: :signup
-  post '/complete',     to: 'public#complete',  as: :complete
 
   get 'blog', to: "articles#index"
   get 'beta', to: "beta_users#index"
