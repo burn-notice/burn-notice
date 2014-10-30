d7d54259a288c1e54c26a619a311a191153a493a
phoet <phoetmail@googlemail.com>
2014-10-30 14:16:15 +0000

    use german defaults

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,11 @@ module BurnNotice
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :de
 
     config.assets.paths << Rails.root.join("app/assets/fonts")
     config.assets.paths << Emoji.images_path
@@ -26,8 +26,8 @@ module BurnNotice
 
     config.generators do |g|
       g.test_framework  :rspec, fixture: false
-      # g.view_specs      false
-      # g.helper_specs    false
+      g.view_specs      false
+      g.helper_specs    false
     end
   end
 end
