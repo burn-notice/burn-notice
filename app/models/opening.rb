7f1c65767d5393522df259366c94368ed4a8f10d
phoet <phoetmail@googlemail.com>
2015-06-09 22:30:08 +0200

    fix typo

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -1,7 +1,7 @@
 class Opening < ActiveRecord::Base
   belongs_to :notice
 
-  validate :notice, :meta, :ip, presence: true
+  validates :notice, :meta, :ip, presence: true
 
   enum authorization: {requested: 0, authorized: 1, unauthorized: 2}
 end
