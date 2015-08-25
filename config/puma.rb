3c151d90a2823d2cc4ebe539f99c9e25835c84b5
phoet <phoetmail@googlemail.com>
2015-08-25 22:42:14 +0200

    lower concurrency

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV['PUMA_WORKERS'] || 2)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 4)
 
 preload_app!
 
@@ -11,10 +11,7 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
+    config['pool'] = ENV['MAX_THREADS'] || 4
     ActiveRecord::Base.establish_connection(config)
   end
-  # if defined?(Resque)
-  #   Resque.redis = ENV["<redis-uri>"] || "redis://127.0.0.1:6379"
-  # end
 end
