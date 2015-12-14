fd46af842a532acff5dadc23411597b625f7b3ed
phoet <phoetmail@googlemail.com>
2015-12-14 16:55:42 +0100

    same host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {host: 'burn-notice.me', protocol: :https}
+  config.default_host = 'www.burn-notice.me'
+  config.action_mailer.default_url_options = {host: config.default_host, protocol: :https}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
@@ -84,7 +85,6 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  config.default_host = 'www.burn-notice.me'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
