b7c060e7daa1a07be1ae829152b1782c14b4f956
phoet <phoetmail@googlemail.com>
2015-12-23 13:33:43 +0100

    show a progress indicator for turbolink loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
 //= require bootstrap.min
 //= require custom
 //= require analytics
