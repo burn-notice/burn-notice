ee9bddcc7db42168a1e1e0c783533824e1f3fc7c
phoet <phoetmail@googlemail.com>
2016-01-29 23:14:51 +0100

    add title

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def gravatar(user)
-    image_tag "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.strip.downcase)}", alt: user.nickname
+    image_tag "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.strip.downcase)}", alt: user.nickname, title: user.nickname
   end
 end
