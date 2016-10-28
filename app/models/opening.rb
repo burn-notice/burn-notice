295f2e4238ff75f3f2bc217565e2ffab32341f95
phoet <phoetmail@googlemail.com>
2016-10-28 22:22:45 +0200

    ugly presentation of meta infos

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -4,4 +4,12 @@ class Opening < ActiveRecord::Base
   validates :notice, :meta, :ip, presence: true
 
   enum authorization: {requested: 0, authorized: 1, unauthorized: 2}
+
+  def referer
+    meta["referer"]
+  end
+
+  def user_agent
+    meta["user_agent"]
+  end
 end
